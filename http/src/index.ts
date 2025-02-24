409146a80af52dab86751a84a30f808cc2bc1a83
Prasanth-S7 <prasanthsampath2005@gmail.com>
2025-02-25 04:57:34 +0530

    ssl

--- http/src/index.ts
+++ http/src/index.ts
@@ -1,18 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import https from 'https';  // Import HTTPS module
+import fs from 'fs';
 import { PORT } from './config';
 import { userRouter } from './routes/user';
 import { chatRouter } from './routes/chat';
 import { forumRouter } from './routes/forum';
 import eventRouter from './routes/event/event';
-import morgan from 'morgan'
+import morgan from 'morgan';
 import { postRouter } from './routes/post';
 import cookieParser from 'cookie-parser';
 import router from './routes/router';
 
 const app = express();
 
-app.use(morgan('dev'))
+const privateKey = fs.readFileSync('../ssl/server.key', 'utf8');
+const certificate = fs.readFileSync('../ssl/server.crt', 'utf8');  
+const credentials = { key: privateKey, cert: certificate };
+
+app.use(morgan('dev'));
 app.use(cors({
     origin: ["http://localhost:5173", "http://172.16.59.133:5173", "https://arize.pages.dev"], 
     credentials: true,
@@ -21,9 +27,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/uploads', express.static('uploads'));
-
 app.use('/api/v1', router);
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-})
+// Create an HTTPS server
+https.createServer(credentials, app).listen(PORT, () => {
+    console.log(`🚀 HTTPS Server started on port ${PORT}`);
+});
